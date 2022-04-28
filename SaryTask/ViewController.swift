893cbc92e9140cecc7bc562f13057b8cfaedbd5b
mohamed abdalla <mohamed.shera.external@atos.net>
2022-04-28 16:42:01 +0200

    Initial Commit

---
+++ SaryTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaryTask
+//
+//  Created by mohamed shera on 28/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SaryTaskTests/SaryTaskTests.swift
@@ -0,0 +1,33 @@
+//
+//  SaryTaskTests.swift
+//  SaryTaskTests
+//
+//  Created by mohamed shera on 28/04/2022.
+//
+
+import XCTest
+@testable import SaryTask
+
+class SaryTaskTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
